3e3b722ee622840d235f44f451af2b317ad0b2be
frederic Boileau <frederic.boileau@protonmail.com>
2020-04-04 14:32:07 -0400

    first version

---
+++ .auctex-auto/master.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "master"
+ (lambda ()
+   (TeX-run-style-hooks
+    "prereg"
+    "pills"
+    "brunch"
+    "morning"
+    "dialoguePostNid"
+    "fontspec"
+    "dramatist"))
+ :latex)
+
